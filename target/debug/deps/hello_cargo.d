d7e4c4292a0f9fdb771dc6a4f283a620fde56ff3
Becka7 <82467937+Becka7@users.noreply.github.com>
2022-04-26 14:45:33 +0300

    create new cargo file

---
+++ target/debug/deps/hello_cargo.d
@@ -0,0 +1,5 @@
+C:\Users\Becca\Desktop\RustPrac\hello_cargo\target\debug\deps\hello_cargo.exe: src\main.rs
+
+C:\Users\Becca\Desktop\RustPrac\hello_cargo\target\debug\deps\hello_cargo.d: src\main.rs
+
+src\main.rs:
---
+++ target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+C:\Users\Becca\Desktop\RustPrac\hello_cargo\target\debug\hello_cargo.exe: C:\Users\Becca\Desktop\RustPrac\hello_cargo\src\main.rs
